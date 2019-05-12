c59b43984d6a9b6df97514368e530fb19f031e9f
stackprobe <stackprobes@gmail.com>
2019-05-13 00:46:24 +0900

    chocolate cornet time

---
+++ Donut2/Donut2/Donut2/Common/CommonEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Common
+{
+	public partial class DD
+	{
+		private class CommonEffect
+		{
+		}
+	}
+}
---
+++ Donut2/Donut2/Donut2/Common/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using DxLibDLL;
+
+namespace Charlotte.Common
+{
+	public partial class DD
+	{
+		private class Main
+		{
+		}
+
+		public static void WinMain()
+		{
+			DX.SetApplicationLogSaveDirectory(@"C:\tmp");
+
+			//new Icon(@"C:\Dat\Icon\game_app.ico").Handle;
+
+			new GameMain().Main();
+
+			// TODO
+		}
+	}
+}
