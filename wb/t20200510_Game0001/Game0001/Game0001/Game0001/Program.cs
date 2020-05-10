c26c5c684ce8ff8321f91c0c25176ae3588168e1
stackprobe <stackprobes@gmail.com>
2020-05-11 01:36:14 +0900

    Milky

---
+++ wb/t20200510_Game0001/Game0001/Game0001/Game0001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{5f99e365-57e0-4b18-84b9-8b0596d88262}";
+		public const string APP_TITLE = "Game0001";
+	}
+}
---
+++ wb/t20200510_Game0001/Game0001/Game0001/_copylib.bat
@@ -0,0 +1,5 @@
+C:\Factory\SubTools\CopyLib.exe C:\Dev\Fairy\Donut2\Donut2\Donut2\Common Game0001\Common
+C:\Factory\SubTools\CopyLib.exe C:\Dev\Game3\Brilliant\Brilliant\Brilliant\Game3Common Game0001\Game3Common
+C:\Factory\SubTools\CsProjAdjustCs.exe Game0001 Common
+C:\Factory\SubTools\CsProjAdjustCs.exe Game0001 Game3Common
+ROBOCOPY C:\Dev\Fairy\Donut2\Resource\General ..\Resource\General /MIR
