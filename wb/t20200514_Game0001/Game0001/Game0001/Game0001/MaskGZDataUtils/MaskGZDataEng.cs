699b1185c2a644fd7d0490faa743ecf5d77660c3
stackprobe <stackprobes@gmail.com>
2020-05-29 03:00:40 +0900

    Rhythm

--- wb/t20200514_Game0001/Game0001/Game0001/Game0001/MaskGZDataUtils/MaskGZDataEng.cs
+++ wb/t20200514_Game0001/Game0001/Game0001/Game0001/MaskGZDataUtils/MaskGZDataEng.cs
@@ -9,53 +9,57 @@ namespace Charlotte.MaskGZDataUtils
 {
 	// sync > @ MakeGZDataEng
 
-	public class MaskGZDataEng
-	{
-		private uint X;
-
-		private uint Rand()
-		{
-			// Xorshift-32
-
-			this.X ^= this.X << 13;
-			this.X ^= this.X >> 17;
-			this.X ^= this.X << 5;
-
-			return this.X;
-		}
-
-		private void Shuffle(int[] values)
-		{
-			for (int index = values.Length; 2 <= index; index--)
-			{
-				int a = index - 1;
-				int b = (int)(Rand() % (uint)index);
-
-				int tmp = values[a];
-				values[a] = values[b];
-				values[b] = tmp;
-			}
-		}
-
-		public void Transpose(byte[] data, uint seed = 123456789)
-		{
-			int[] swapIdxLst = Enumerable.Range(1, data.Length / 2).ToArray();
-
-			this.X = seed;
-			this.Shuffle(swapIdxLst);
-			this.X = 0; // clear
-
-			for (int index = 0; index < swapIdxLst.Length; index++)
-			{
-				int a = index;
-				int b = data.Length - swapIdxLst[index];
-
-				byte tmp = data[a];
-				data[a] = data[b];
-				data[b] = tmp;
-			}
-		}
-	}
+	// $_git:secretBegin
+//
+	//////////////////////////
+	//
+		///////////////
+//
+		///////////////////
+		//
+			//////////////
+//
+			///////////////////////
+			///////////////////////
+			//////////////////////
+//
+			//////////////
+		//
+//
+		//////////////////////////////////
+		//
+			////////////////////////////////////////////////////
+			//
+				//////////////////
+				////////////////////////////////////
+//
+				////////////////////
+				//////////////////////
+				////////////////
+			//
+		//
+//
+		/////////////////////////////////////////////////////////
+		//
+			//////////////////////////////////////////////////////////////////
+//
+			//////////////
+			/////////////////////////
+			////////////////////
+//
+			///////////////////////////////////////////////////////
+			//
+				//////////////
+				////////////////////////////////////////
+//
+				///////////////////
+				//////////////////
+				//////////////
+			//
+		//
+	//
+//
+	// $_git:secretEnd
 
 	// < sync
 }
