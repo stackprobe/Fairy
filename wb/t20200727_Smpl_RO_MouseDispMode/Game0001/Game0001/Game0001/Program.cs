34b7acfd3b9e3e61cb1b0da4277e69e25ae4689e
stackprobe <stackprobes@gmail.com>
2020-07-28 08:51:29 +0900

    Luminous

---
+++ wb/t20200727_Smpl_RO_MouseDispMode/Game0001/Game0001/Game0001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{727c5931-1401-472e-b855-e88e6b38de45}";
+		public const string APP_TITLE = "Game0001";
+	}
+}
