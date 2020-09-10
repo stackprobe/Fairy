28344eb5c76e63374c8755f0c0e113139995f8d7
stackprobe <stackprobes@gmail.com>
2020-09-10 10:36:36 +0900

    Europa

---
+++ wb/t20200909_Smpl_Mouse/Game0001/Game0001/Game0001/Games/GameTest0002.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Common;
+using Charlotte.Tools;
+
+namespace Charlotte.Games
+{
+	public class GameTest0002
+	{
+		private int WARP_L = 600;
+		private int WARP_T = 400;
+		private int WARP_W = 100;
+		private int WARP_H = 100;
+
+		private int WARP_DEST_X = 200;
+		private int WARP_DEST_Y = 300;
+
+		private int CROSS_WH = 100;
+
+		public void Perform()
+		{
+			DDEngine.FreezeInput(10);
+
+			for (; ; )
+			{
+				DDMouse.UpdatePos();
+
+				if (DDUtils.IsOut(new D2Point(DDMouse.X, DDMouse.Y), new D4Rect(WARP_L, WARP_T, WARP_W, WARP_H)) == false)
+				{
+					DDMouse.X = WARP_DEST_X;
+					DDMouse.Y = WARP_DEST_Y;
+
+					DDMouse.ApplyPos();
+				}
+				if (DDMouse.L.GetInput() == -1)
+					break;
+
+				DDCurtain.DrawCurtain();
+
+				DDPrint.SetPrint(0, 0, 24);
+				DDPrint.PrintLine("★位置取得と位置設定");
+				DDPrint.PrintLine("X=" + DDMouse.X);
+				DDPrint.PrintLine("Y=" + DDMouse.Y);
+				DDPrint.PrintLine("左クリックでメニューに戻る。");
+				DDPrint.PrintLine("水色の領域にマウスカーソルを移動すると、黄色い十字にワープする。");
+
+				DDDraw.SetBright(new I3Color(0, 200, 200));
+				DDDraw.DrawRect(DDGround.GeneralResource.WhiteBox, WARP_L, WARP_T, WARP_W, WARP_H);
+				DDDraw.Reset();
+
+				DDDraw.SetBright(new I3Color(200, 200, 0));
+				DDDraw.DrawRect(DDGround.GeneralResource.WhiteBox, WARP_DEST_X, WARP_DEST_Y - CROSS_WH / 2, 1, CROSS_WH);
+				DDDraw.DrawRect(DDGround.GeneralResource.WhiteBox, WARP_DEST_X - CROSS_WH / 2, WARP_DEST_Y, CROSS_WH, 1);
+				DDDraw.Reset();
+
+				DDEngine.EachFrame();
+			}
+		}
+	}
+}
---
+++ wb/t20200909_Smpl_Mouse/Game0001/Game0001/Game0001/Games/TitleMenu.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Tools;
+using Charlotte.Common;
+
+namespace Charlotte.Games
+{
+	public class TitleMenu
+	{
+		private DDSimpleMenu SimpleMenu;
+
+		public void Perform()
+		{
+			DDCurtain.SetCurtain();
+			DDEngine.FreezeInput();
+
+			//Ground.I.Music.Title.Play();
+
+			string[] items = new string[]
+			{
+				"テスト01",
+				"テスト02",
+				"テスト03",
+				"設定",
+				"終了",
+			};
+
+			int selectIndex = 0;
+
+			this.SimpleMenu = new DDSimpleMenu();
+
+			this.SimpleMenu.WallColor = new I3Color(0, 0, 64);
+			//this.SimpleMenu.WallPicture = Ground.I.Picture.TitleWall;
+
+			for (; ; )
+			{
+				selectIndex = this.SimpleMenu.Perform("Donut3", items, selectIndex);
+
+				switch (selectIndex)
+				{
+					case 0:
+						this.LeaveTitleMenu();
+						new GameTest0001().Perform();
+						this.ReturnTitleMenu();
+						break;
+
+					case 1:
+						this.LeaveTitleMenu();
+						new GameTest0002().Perform();
+						this.ReturnTitleMenu();
+						break;
+
+					case 2:
+						this.LeaveTitleMenu();
+						new GameTest0003().Perform();
+						this.ReturnTitleMenu();
+						break;
+
+					case 3:
+						this.Setting();
+						break;
+
+					case 4:
+						goto endMenu;
+
+					default:
+						throw new DDError();
+				}
+			}
+		endMenu:
+			DDEngine.FreezeInput();
+			DDMusicUtils.Fade();
+			DDCurtain.SetCurtain(30, -1.0);
+
+			foreach (DDScene scene in DDSceneUtils.Create(40))
+			{
+				this.DrawWall();
+				DDEngine.EachFrame();
+			}
+		}
+
+		private void Setting()
+		{
+			DDCurtain.SetCurtain();
+			DDEngine.FreezeInput();
+
+			string[] items = new string[]
+			{
+				"パッドのボタン設定",
+				"ウィンドウサイズ変更",
+				"ＢＧＭ音量",
+				"ＳＥ音量",
+				"戻る",
+			};
+
+			int selectIndex = 0;
+
+			for (; ; )
+			{
+				selectIndex = this.SimpleMenu.Perform("設定", items, selectIndex);
+
+				switch (selectIndex)
+				{
+					case 0:
+						this.SimpleMenu.PadConfig();
+						break;
+
+					case 1:
+						this.SimpleMenu.WindowSizeConfig();
+						break;
+
+					case 2:
+						this.SimpleMenu.VolumeConfig("ＢＧＭ音量", DDGround.MusicVolume, 0, 100, 1, 10, volume =>
+						{
+							DDGround.MusicVolume = volume;
+							DDMusicUtils.UpdateVolume();
+						},
+						() => { }
+						);
+						break;
+
+					case 3:
+						this.SimpleMenu.VolumeConfig("ＳＥ音量", DDGround.SEVolume, 0, 100, 1, 10, volume =>
+						{
+							DDGround.SEVolume = volume;
+							DDSEUtils.UpdateVolume();
+						},
+						() =>
+						{
+							//Ground.I.SE.XXXXX.Play();
+						}
+						);
+						break;
+
+					case 4:
+						goto endMenu;
+
+					default:
+						throw new DDError();
+				}
+			}
+		endMenu:
+			DDEngine.FreezeInput();
+		}
+
+		private void DrawWall()
+		{
+			DDDraw.SetBright(new I3Color(0, 0, 64));
+			DDDraw.DrawRect(DDGround.GeneralResource.WhiteBox, 0, 0, DDConsts.Screen_W, DDConsts.Screen_H);
+			DDDraw.Reset();
+			//DDDraw.DrawRect(Ground.I.Picture.TitleWall, 0, 0, DDConsts.Screen_W, DDConsts.Screen_H);
+		}
+
+		private void LeaveTitleMenu()
+		{
+#if false
+			DDMusicUtils.Fade();
+			DDCurtain.SetCurtain(30, -1.0);
+
+			foreach (DDScene scene in DDSceneUtils.Create(40))
+			{
+				this.DrawWall();
+				DDEngine.EachFrame();
+			}
+#endif
+
+			GC.Collect();
+		}
+
+		private void ReturnTitleMenu()
+		{
+			//Ground.I.Music.Title.Play();
+
+			GC.Collect();
+		}
+	}
+}
